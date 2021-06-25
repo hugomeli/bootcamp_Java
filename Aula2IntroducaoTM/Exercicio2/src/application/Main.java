8ee30b79b8167ac60081525c9ae73a945036d106
huribeiro <hugo.ribeiro@mercadolivre.com>
2021-06-25 15:57:20 -0300

    Aula2TM - Exercicio 3

--- Aula2IntroducaoTM/Exercicio2/src/application/Main.java
+++ Aula2IntroducaoTM/Exercicio2/src/application/Main.java
@@ -10,14 +10,19 @@ import entities.Counter;
 import exceptions.IncDecException;
 
 public class Main {
-    public static void main(String[] args) throws IncDecException {
+    public static void main(String[] args) {
         Counter counter1 = new Counter();
         Counter counter2 = new Counter(45);
         Counter counter3 = new Counter(counter2);
 
         System.out.println("Contador 2: " + counter2.getNumeroContagem());
         System.out.println("Contador 3: " + counter3.getNumeroContagem());
-        counter2.aumentaContagem(10);
+        try{
+            counter2.aumentaContagem(10);
+        }catch(IncDecException ex){
+            System.out.println(ex.getMessage());
+        }
+
         System.out.println("Contador 2: " + counter2.getNumeroContagem());
 
     }
