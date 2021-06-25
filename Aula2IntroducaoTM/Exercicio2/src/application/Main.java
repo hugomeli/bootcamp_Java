baa2f3d0a0817b114ab9949997d46d7e29944013
huribeiro <hugo.ribeiro@mercadolivre.com>
2021-06-25 15:05:53 -0300

    Aula 2 TM - Exercicio 2 - Individual

---
+++ Aula2IntroducaoTM/Exercicio2/src/application/Main.java
@@ -0,0 +1,25 @@
+/*
+Crie uma classe Counter (sim, contador de programação) com métodos que permitam aumentar e diminuir seu valor.
+A classe conterá um construtor padrão, um construtor com parâmetros, um construtor de cópia e os setters e getters
+correspondentes (métodos de acesso).
+ */
+
+package application;
+
+import entities.Counter;
+import exceptions.IncDecException;
+
+public class Main {
+    public static void main(String[] args) throws IncDecException {
+        Counter counter1 = new Counter();
+        Counter counter2 = new Counter(45);
+        Counter counter3 = new Counter(counter2);
+
+        System.out.println("Contador 2: " + counter2.getNumeroContagem());
+        System.out.println("Contador 3: " + counter3.getNumeroContagem());
+        counter2.aumentaContagem(10);
+        System.out.println("Contador 2: " + counter2.getNumeroContagem());
+
+    }
+
+}
