2af3373a9e2d942d966990b11002e2fd9bf9e69e
huribeiro <hugo.ribeiro@mercadolivre.com>
2021-06-25 14:35:12 -0300

    Aula 2 TM - Exercicio 1 - Individual

---
+++ Aula2IntroducaoTM/Exercicio1/src/entities/ContaCorrente.java
@@ -0,0 +1,69 @@
+package entities;
+
+import exception.DepositoException;
+import exception.SaqueException;
+
+public class ContaCorrente {
+
+    private double saldo;
+    private String numeroConta;
+    private String nomeTitular;
+
+    public ContaCorrente() {
+    }
+
+    public ContaCorrente(double saldo, String numeroConta, String nomeTitular){
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+    }
+
+    public ContaCorrente(ContaCorrente contaParametro){
+        this.saldo = contaParametro.getSaldo();
+        this.numeroConta = contaParametro.getNumeroConta();
+        this.nomeTitular = contaParametro.getNomeTitular();
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public void deposita(double valor) throws DepositoException {
+        if (valor < 0){
+            throw new DepositoException("O valor do depósito não pode ser negativo!");
+        }
+        this.saldo += valor;
+    }
+
+    public void saca(double valor) throws SaqueException{
+        if (this.getSaldo() < valor){
+            throw new SaqueException("Valor do saldo insuficiente para efetuar esse saque!" +
+                                    "Saldo atual: " + this.getSaldo());
+        }
+        this.saldo -= valor;
+    }
+
+    @Override
+    public String toString() {
+        return  "\nNumero da Conta = " + numeroConta +
+                "\nSaldo = " + saldo +
+                "\nNome do Titular = " + nomeTitular + '\n'
+                ;
+    }
+}
