0a282fce95d500daa41d4a62fe6ad21cf4e6a6db
huribeiro <hugo.ribeiro@mercadolivre.com>
2021-06-25 18:31:44 -0300

    Aula2TM - Exercicio 4 - Corrigindo métodos

---
+++ Aula2IntroducaoTM/Exercicio5/src/application/Main.java
@@ -0,0 +1,11 @@
+/*
+Crie uma classe Data. A classe conterá, além dos construtores que você considera apropriados,
+métodos de acesso e o método toString, conforme explicamos no exercício anterior,
+um método para verificar se a data está correta e outro para adicionar um dia ao valor atual da data.
+A classe GregorianCalendar deve ser investigada e usada para implementar os construtores e métodos Date.
+ */
+
+package application;
+
+public class Main {
+}
---
+++ Aula2IntroducaoTM/Exercicio5/src/entities/Data.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Data {
+
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(){
+
+    }
+
+
+}
